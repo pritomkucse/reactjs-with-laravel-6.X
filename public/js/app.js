955282151fe9b57a4443ee1efd8923b99b7d17c2
Pritom K Mondal <pritomkucse@gmail.com>
2021-03-17 14:59:13 +0600

    Development Process

--- public/js/app.js
+++ public/js/app.js
@@ -43,7 +43,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "js/chunk/" + ({}[chunkId]||chunkId) + "-1615971321009.js"
+/******/ 		return __webpack_require__.p + "js/chunk/" + ({}[chunkId]||chunkId) + "-1615971491747.js"
 /******/ 	}
 /******/
 /******/ 	// The require function
